ebce597d5e0a161b6b15ac659b0f72b6002d7308
Sofiag8 <dianasofiagarciacaicedo@gmail.com>
2022-11-14 16:27:25 -0500

    feat: add some basic routes checking on express crud

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
+const fs = require('fs')
 const express = require("express")
 
 const app = express()
 const port = 3000
 
-app.get("/", (request, response) => {
-    response.status(200).json({message: 'Hello from the server side', app: 'natours'})
+// middleware
+app.use(express.json())
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get("/api/v1/tours", (request, response) => {
+    response.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
 })
 
-app.post("/", (request, response) => {
-    response.send("You can post this endpoint")
+app.get("/api/v1/tours/:id", (request, response) => {
+    const tour = tours.find(el => el.id === Number(request.params.id))
+    if (!tour) {
+        return response.status(404).json({
+            status: 'failed',
+            message: 'Id could not be found on records'
+        })
+    }
+    console.log(tour)
+    response.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+})
+
+app.post("/api/v1/tours", (request, response) => {
+
+    const newId = tours[tours.length -1].id  + 1
+    const newTour = Object.assign({id: newId}, request.body)
+
+    tours.push(newTour)
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        response.status(201).json({
+            status: 'success',
+            data: {
+                tour:newTour
+            }
+        })
+    })
 })
 
 app.listen(port, ()=> {
